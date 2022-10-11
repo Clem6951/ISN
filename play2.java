c193342cef1f7ba6194bc3e27fa1f71fe9c55d9f
Clem6951 <cetienne88220@gmail.com>
2022-10-11 15:15:36 +0200

    gefg

---
+++ play2.java
@@ -0,0 +1,9 @@
+package simple2;
+
+public class play2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Smells like updog in here");
+	}
+}
